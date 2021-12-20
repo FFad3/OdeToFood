2353c64c00ca21fadacdd9db6fadbac951647dee
FFad3 <gnebapatryk@gmail.com>
2021-12-20 17:33:01 +0100

    VSCode

---
+++ OdeToFood.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class OdeToFoodModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
